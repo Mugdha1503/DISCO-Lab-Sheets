29b8b0f00bd0abc275c92d397357ca99218c0f48
Mugdha Gupta <111490641+Mugdha1503@users.noreply.github.com>
2024-01-01 18:29:37 +0530

    Add files via upload

---
+++ A3/q7.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+const int N=1e5+10;
+vector<int> g[N];
+bool vis[N];
+void dfs(int vertex){
+    vis[vertex]=1;
+    for(int child:g[vertex]){
+        if(vis[child]) continue;
+        dfs(child);
+    }
+}
+int main()
+{    
+    int n,m;
+    cin>>n>>m;
+    int ct=0;
+    for(int i=0;i<m;i++){
+        int v1,v2;
+        cin>>v1>>v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
+    }
+    for(int i=1;i<=n;i++){
+        if(vis[i]) continue;
+        dfs(i);
+        ct++;
+    }
+    cout<<ct<<endl;
+    return 0;
+}
+
+
+
+
